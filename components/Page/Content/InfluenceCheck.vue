1f086b481d8946060d9a123ce53c99e9634a5a7b
9777717 <you@example.com>
2023-04-13 11:45:20 +0800

    fix: 视光检查

---
+++ components/Page/Content/InfluenceCheck.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="InfluenceCheck">
+    <div>視光檢查</div>
+    <div>
+      <div>
+        <div>
+          <div>
+            <img
+              src="https://static.cmereye.com/imgs/2023/04/fe71351b6af68252.png"
+              alt="視光檢查"
+            />
+          </div>
+          <div>
+            <div>綜合檢查</div>
+            <div>
+              8歲以下一般視為兒童視力發展至關重要的時期。若眼睛的發育在這期間出現異常而沒有作出矯正，便可能造成難以挽救的視力損害。因此，家長應為兒童定期安排綜合眼睛檢查，掌握小孩的視覺發展需要。
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss"></style>
